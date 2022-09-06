0f79e8ec773bac9880d9031721e2260906483092
Ashiqul <ashiqul33@gmail.com>
2022-09-07 00:43:59 +0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+
+import "../styles/cart_list.scss";
+import "../styles/common.scss";
+import "../styles/course_details.scss";
+import "../styles/home.scss";
+import "../styles/login.scss";
+import "../styles/profile.scss";
+import "../styles/signup.scss";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
